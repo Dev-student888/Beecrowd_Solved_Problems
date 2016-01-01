5669720caf39655de1a77d777b0ff3ebabaed75f
Ian <ian.anime@outlook.com.br>
2016-01-01 15:17:22 -0200

    Create 1004.cpp

---
+++ 1004.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ 
+    int A, B, PROD;
+    scanf("%i", &A);
+    scanf("%i", &B);
+    PROD = A * B;
+    printf("PROD = %i\n", PROD);
+    return 0;
+}
