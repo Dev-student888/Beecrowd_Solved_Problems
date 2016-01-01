56c1ae39a7c9b9cd0460362a6e7c96b96deab80e
Ian <ian.anime@outlook.com.br>
2016-01-01 15:13:00 -0200

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int A, B, X;
+//    printf("Entre com o valor de A: ");
+    scanf("%d", &A);
+//    printf("Entre com o valor de B: ");
+    scanf("%d", &B);
+    X = A + B;
+    printf("X = %d\n",X);
+    return 0;
+}
