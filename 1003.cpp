9b0b6f0a0b8604592761229fc3615bb3477ec272
Ian <ian.anime@outlook.com.br>
2016-01-01 15:15:46 -0200

    Create 1003.cpp

---
+++ 1003.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ 
+    int A, B, S;
+    scanf("%i", &A);
+    scanf("%i", &B);
+    S = A + B;
+    printf("SOMA = %i\n", S);
+    return 0;
+}
